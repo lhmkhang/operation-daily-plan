2c9dd76a0e4cb9737b93c5efe3d4d6bfda64ba9c
unknown <lhmkhang@digi-texx.vn>
2023-07-23 12:13:46 +0700

    Add router upload-volume

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,52 +1,88 @@
-const jwt = require('jsonwebtoken');
-const { getDatabase } = require('../middlewaves/getDatabase.js');
-const logger = require('../middlewaves/logger.js');
+const jwt = require("jsonwebtoken");
+const XLSX = require("xlsx");
+const fs = require("fs");
+const { getDatabase } = require("../middlewaves/getDatabase.js");
+const logger = require("../middlewaves/logger.js");
 
-const loggerError = logger.getLogger('errorLogger');
-const loggerInfo = logger.getLogger('infoLogger');
+const loggerError = logger.getLogger("errorLogger");
+const loggerInfo = logger.getLogger("infoLogger");
 
 const getNavbarItem = async (req, res) => {
-
-    try {
-        const database = await getDatabase("operation")
-        const collection = database.collection('menu_item')
-        const list_item = await collection.findOne({}, { projection: { _id: 0 } });
-        return res.json(list_item);
-    } catch (error) {
-        loggerError.error(error);
-    }
-
+  try {
+    const database = await getDatabase("operation");
+    const collection = database.collection("menu_item");
+    const list_item = await collection.findOne({}, { projection: { _id: 0 } });
+    return res.json(list_item);
+  } catch (error) {
+    loggerError.error(error);
+  }
 };
 
 const getRoot = (req, res) => {
-    return res.send("Welcome to backend express server!")
+  return res.send("Welcome to backend express server!");
 };
 
 const getUser = async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+  const username = req.body.username;
+  const password = req.body.password;
 
-    try {
-        const database = await getDatabase('operation');
-        const userCollection = database.collection('users');
-        const user = await userCollection.findOne({ username: username });
+  try {
+    const database = await getDatabase("operation");
+    const userCollection = database.collection("users");
+    const user = await userCollection.findOne({ username: username });
 
-        if (!user || user.password !== password) {
-            loggerError.error('Invalid username or password');
-            res.status(400).json({ error: 'Invalid username or password' });
-        } else {
+    if (!user || user.password !== password) {
+      loggerError.error("Invalid username or password");
+      res.status(400).json({ error: "Invalid username or password" });
+    } else {
+      loggerInfo.info("Login successful");
 
-            loggerInfo.info('Login successful');
-
-            const token = jwt.sign({ username }, process.env.SECRET_KEY, { expiresIn: process.env.EXPIRE_TOKEN_IN })
-            req.session.user = user;
-            req.session.user.group = user.group;
-            res.cookie('user_id', user._id, { maxAge: Number(process.env.SESSION_LIFE_TIME) });
-            res.json({ token, expiresIn: process.env.EXPIRE_TOKEN_IN, receivedTime: new Date() });
-        }
-    } catch (err) {
-        loggerError.error(err);
+      const token = jwt.sign({ username }, process.env.SECRET_KEY, {
+        expiresIn: process.env.EXPIRE_TOKEN_IN,
+      });
+      req.session.user = user;
+      req.session.user.group = user.group;
+      res.cookie("user_id", user._id, {
+        maxAge: Number(process.env.SESSION_LIFE_TIME),
+      });
+      res.json({
+        token,
+        expiresIn: process.env.EXPIRE_TOKEN_IN,
+        receivedTime: new Date(),
+      });
     }
+  } catch (err) {
+    loggerError.error(err);
+  }
 };
 
-module.exports = { getNavbarItem, getRoot, getUser };
+const uploadVolume = (req, res) => {
+  // const workbook = XLSX.utils.book_new();
+  try {
+    let readStream = fs.createReadStream(req.file.path);
+    let data = [];
+
+    readStream.on("data", (chunk) => {
+      data.push(chunk);
+    });
+
+    readStream.on("end", async () => {
+      const buffer = Buffer.concat(data);
+      const workbook = XLSX.read(buffer, { type: "buffer" });
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, {
+        header: 0,
+        defval: "",
+      });
+      // const csvData = XLSX.utils.sheet_to_csv(worksheet)
+
+      console.log(jsonData);
+    });
+    return res.json("File uploaded successful!");
+  } catch (error) {
+    loggerError.error("Error while processing the uploaded file:", error);
+    return res.status(500).json("File processing failed.");
+  }
+};
+
+module.exports = { getNavbarItem, getRoot, getUser, uploadVolume };
--- src/routers/router.js
+++ src/routers/router.js
@@ -1,15 +1,36 @@
 const express = require("express");
+const fs = require("fs");
 const homeController = require("../controllers/homeController.js");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 
-let router = express.Router();
+const router = express.Router();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const uploadDir = "uploads";
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    const uniqueFileName = uuidv4() + "-" + file.originalname;
+    cb(null, uniqueFileName);
+  },
+});
+const upload = multer({ storage: storage });
 
 let initWebRoutes = (app) => {
+  router.get("/", homeController.getRoot);
+  router.get("/navbar-item", homeController.getNavbarItem);
+  router.post("/login", homeController.getUser);
+  router.post(
+    "/upload-volume",
+    upload.single("file"),
+    homeController.uploadVolume
+  );
 
-    router.get("/", homeController.getRoot);
-    router.get("/navbar-item", homeController.getNavbarItem);
-    router.post("/login", homeController.getUser);
-
-    return app.use("/", router);
-}
+  return app.use("/", router);
+};
 
 module.exports = initWebRoutes;
