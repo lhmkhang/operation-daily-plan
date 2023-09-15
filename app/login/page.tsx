cbfe81eefb501565666fb0f31aa90c295c338a66
ntquoc <nt.quoc1709@gmail.com>
2023-09-15 15:34:21 +0700

    Update source

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,32 @@
 'use client'
+import Image from 'next/image';
 import * as React from 'react';
-
+import TextField from '@mui/material/TextField';
 
 type Props = {}
 
 const Login = (props: Props) => {
+
+    const [isLogin, setIsLogin] = React.useState(true);
+
     return (
-        <div className='container mx-auto'>
-            
+        <div className="w-screen h-screen flex items-center bg-[url('/img/backgroundLogin2.jpg')] bg-cover">
+            <div className="container m-auto h-2/4 w-1/2 px-4 rounded-2xl bg-gray-100">
+                <div className="grid grid-cols-5 h-full">
+                    <div className='border-2 border-black col-span-3'>
+                        <Image
+                            alt='Image Login'
+                            src={'/img/imageLogin2.jpg'}
+                            width={600}
+                            height={600}
+                            style={{ width: '100%', height: '100%', backgroundSize: 'cover' }}
+                        />
+                    </div>
+                    <div className='border-2 border-black col-span-2 flex items-center justify-center'>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
 
 export default function Home() {
   return (
