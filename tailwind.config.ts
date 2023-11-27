4f4067372c88e0e4b52e2692cb606dd4d681a8e3
Khang Le Hoang Minh <lhmkhang@digi-texx.local>
2023-11-27 08:59:42 +0700

    fix css issue when loading login form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  important: true,
   theme: {
     extend: {
       backgroundImage: {
