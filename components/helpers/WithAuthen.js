c4803ca42c0bbc3644e5e81b3012bfdce04b2583
Khang Le Hoang Minh <lhmkhang@digi-texx.local>
2023-12-01 17:33:12 +0700

    fix css for responsive lucky wheel

--- components/helpers/WithAuthen.js
+++ components/helpers/WithAuthen.js
@@ -2,74 +2,77 @@
 import React, { useContext, useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import { AuthContext } from "../helpers/AuthenContext";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import axios from "axios";
 
 function withAuth(WrappedComponent) {
-  function WithAuthComponent(props) {
-    const router = useRouter();
-    const { user, setUser } = useContext(AuthContext);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isNavigating, setIsNavigating] = useState(false);
+    function WithAuthComponent(props) {
+        const router = useRouter();
+        const { user, setUser } = useContext(AuthContext);
+        const [isLoading, setIsLoading] = useState(true);
+        const [isNavigating, setIsNavigating] = useState(false);
 
-    useEffect(() => {
-      let accessToken;
-      let refreshToken;
-      let username;
+        console.log("URL: ", usePathname());
 
-      console.log(`User in WithAuthen HOC: ${user}`);
+        useEffect(() => {
+            let accessToken;
+            let refreshToken;
+            let username;
 
-      const checkTokenAndRefresh = async () => {
-        try {
-          if (!user) {
-            setIsNavigating(true);
-            router.push("/login");
-          } else {
-            accessToken = JSON.parse(user).accessToken;
-            refreshToken = JSON.parse(user).refreshToken;
-            username = JSON.parse(user).username;
-            const decodedToken = jwtDecode(accessToken);
-            const currentTime = Date.now() / 1000;
+            console.log(`User in WithAuthen HOC: ${user}`);
 
-            if (decodedToken.exp < currentTime) {
-              setIsLoading(true);
-              const response = await axios.post(
-                `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/refresh-token`,
-                { refreshToken, username }
-              );
-              const newAccessToken = response.data.newAccessToken;
-              let tokenData = JSON.parse(sessionStorage.getItem("token"));
-              tokenData.accessToken = newAccessToken;
-              setUser(JSON.stringify(tokenData)); // Cập nhật context
-              sessionStorage.setItem("token", JSON.stringify(tokenData));
-              setIsLoading(false);
-            } else {
-              setIsLoading(false);
-            }
-          }
-        } catch (error) {
-          console.error("Error:", error);
-          setIsNavigating(true);
-          setIsLoading(false);
-          router.push("/login");
+            const checkTokenAndRefresh = async () => {
+                try {
+                    if (!user) {
+                        setIsNavigating(true);
+                        router.push("/login");
+                    } else {
+                        accessToken = JSON.parse(user).accessToken;
+                        refreshToken = JSON.parse(user).refreshToken;
+                        username = JSON.parse(user).username;
+                        const decodedToken = jwtDecode(accessToken);
+                        const currentTime = Date.now() / 1000;
+
+                        if (decodedToken.exp < currentTime) {
+                            setIsLoading(true);
+                            const response = await axios.post(
+                                `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/refresh-token`,
+                                { refreshToken, username }
+                            );
+
+                            const newAccessToken = response.data.newAccessToken;
+                            let tokenData = JSON.parse(sessionStorage.getItem("token"));
+                            tokenData.accessToken = newAccessToken;
+                            setUser(JSON.stringify(tokenData)); // Cập nhật context
+                            sessionStorage.setItem("token", JSON.stringify(tokenData));
+                            setIsLoading(false);
+                        } else {
+                            setIsLoading(false);
+                        }
+                    }
+                } catch (error) {
+                    console.error("Error:", error);
+                    setIsNavigating(true);
+                    setIsLoading(false);
+                    router.push("/login");
+                }
+            };
+
+            checkTokenAndRefresh();
+        }, [user, router, setUser]);
+
+        if (isNavigating) {
+            return null;
         }
-      };
 
-      checkTokenAndRefresh();
-    }, [user, router, setUser]);
+        if (isLoading) {
+            return <div>Loading...</div>; // Hoặc một spinner
+        }
 
-    if (isNavigating) {
-      return null;
+        return <WrappedComponent {...props} />;
     }
 
-    if (isLoading) {
-      return <div>Loading...</div>; // Hoặc một spinner
-    }
-
-    return <WrappedComponent {...props} />;
-  }
-
-  return WithAuthComponent;
+    return WithAuthComponent;
 }
 
 export default withAuth;
